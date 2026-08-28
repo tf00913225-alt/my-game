507db378be50df5cb4c5feaa5d4222653a744eb0
tf00913225-alt <tf00913225@gmail.com>
2026-08-28 21:35:53 +0800

    Finalize V145 wind icon wiring

--- js/20-anonymous-20-v145-wrapper.js
+++
@@ -1,3 +0,0 @@
-/* V145 wrapper: preserve the exact V144 loader, then install wind skill icons only. */
-document.write('<script src="js/20-anonymous-20-v144-original.js?v=144"><\/script>');
-document.write('<script src="js/41-v145-wind-skill-icons.js?v=145"><\/script>');
