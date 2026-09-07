302f947602657e3d9f6cde3824dd953ebaf8597d
tf00913225-alt <tf00913225@gmail.com>
2026-09-07 20:28:31 +0800

    Update starter icon initialization regression

--- tests/v173.57-starter-icons-reforge-filter.test.js
+++ tests/v173.57-starter-icons-reforge-filter.test.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict"
 const assert=require("node:assert/strict");
 const fs=require("node:fs");
 const eq=fs.readFileSync("js/equipment-progression.js","utf8");
@@ -21,7 +21,7 @@ assert.match(eq,/item\.icon=spec\.ring\?legacyStarterRingMarkup\(\):artMarkup\(s
 assert.match(eq,/item\.rarityKey="white"/);
 assert.match(eq,/item\.quality="white"/);
 assert.match(eq,/item\.reforgeSlots=0/);
-assert.match(eq,/repairLegacyStarterEquipmentIcons\(\);\n\s*syncMainCharacterEquipmentStorage\(\)/);
+assert.match(eq,/repairLegacyStarterEquipmentIcons\(\);\n\s*syncEquipmentChestPresentation\(\);\n\s*syncMainCharacterEquipmentStorage\(\)/);
 
 assert.match(synthesis,/function canActuallyReforge\(item\)/);
 assert.match(synthesis,/item\.v17351Locked!==true/);
