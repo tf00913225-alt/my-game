b0c2ad6c61a188f3a65def360ea59c83c0e1b89d
tf00913225-alt <tf00913225@gmail.com>
2026-09-12 19:21:50 +0800

    fix: migrate Android PoC to Kotlin compilerOptions DSL

--- android/facebook-login-poc/app/build.gradle.kts
+++ android/facebook-login-poc/app/build.gradle.kts
@@ -42,12 +42,14 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     buildFeatures {
         buildConfig = true
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
