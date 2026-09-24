b69f58893fa073a70e45de4b6acd6d60b16a99ed
tf00913225-alt <tf00913225@gmail.com>
2026-09-24 22:17:41 +0800

    fix: project legacy auto-battle settings before cloud upload

--- build/boot-core.58924157b4bc.js
+++ build/boot-core.58924157b4bc.js
@@ -1,4 +1,4 @@
-window.__FOUR_SYMBOLS_BUILD__=Object.freeze({"release":"173.72","firebaseBootstrap":"build/firebase/firebase-bootstrap.28cbec0c77c8.js"});
+window.__FOUR_SYMBOLS_BUILD__=Object.freeze({"release":"173.72","firebaseBootstrap":"build/firebase/firebase-bootstrap.39bbb79f8025.js"});
 (function installFourSymbolsSupportContact(global){
 "use strict";
 if(!global||global.FourSymbolsSupport){ return; }
